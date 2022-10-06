5bf71a4e07623323531961f98afbf526c53c567c
Sjoerd <sjoerd-visser@live.nl>
2022-10-06 17:58:31 +0200

    Fix linting issues

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -71,7 +71,7 @@ class RecipeDetailSerializer(RecipeSerializer):
     def create(self, validated_data):
         """Create a recipe."""
         tags = validated_data.pop('tags', [])
-        ingredients = validated_data.pop('ingredients',[])
+        ingredients = validated_data.pop('ingredients', [])
         recipe = Recipe.objects.create(**validated_data)
         self._get_or_create_tags(tags, recipe)
         self._get_or_create_ingredients(ingredients, recipe)
